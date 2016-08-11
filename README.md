e0e41a35da85589357d0aa286c0a1738794f09a3
Aparajita Hait <aparajita.hait@wipro.com>
2016-08-11 09:17:04 +0530

    minor changes

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ractivejstest
 Test Code for Ractive JS Progress Bar
+Link to the GitHub Project:
+https://github.com/dearmimi/ractivejstest
--- tests/test.js
+++ tests/test.js
@@ -5,8 +5,8 @@
 		assert = chai.assert;
 		load = Ractive.load;
 
-it( 'should load a progress-bar component with blank bar', function () {
-			return load( '../templates/progress-bar.html' ).then( function ( Component ) {
+		it( 'should load a progress-bar component with blank bar', function () {
+			return load( '../ractivejs-index.html' ).then( function ( Component ) {
 				var ractive = new Component({
 					data: {	
 						bars:[{name:'progressbar#1', value:0}], 
@@ -17,6 +17,23 @@ it( 'should load a progress-bar component with blank bar', function () {
 				assert.equal( ractive.toHTML(), '<div class="progress-bar-empty" style="width:50%;"><span class="progress-label">0%</span></div> <select value="progressbar#1"><optin value="1"> progress#1</option></select>');
 				
 				
+		});
+
+		it( 'should load a progressive-bar component with red bar', function () {
+			return load( '../ractivejs-index.html' ).then( function ( Component ) {
+				var ractive = new Component({
+					data: {	
+						bars:[{name:'progressbar#1', level:100}], 
+						amounts: [{:25}]
+					}
+				});
+
+				assert.equal( ractive.toHTML(), '<div class="progress-bar-fill" style="width:50%;"><span class="progress-label">0%</span></div> <select value="progressbar#1"><optin value="1"> progress#1</option></select>');
+				
 			});
 			
 		});
+			
+	});
+
+}());
